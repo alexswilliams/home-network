c6ee4d77a14c2fb95f2e4a7a38216874d53b360a
Alex Williams <lexw@live.co.uk>
2021-02-28 17:36:55 +0100

    Firewall tweaks

--- pi-bootstrap.sh
+++ pi-bootstrap.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+
+# Pre-tasks:
+# - Installed base OS onto card
+# - Have SSH'd to it at least once (sshpass doesn't support missing hostkeys)
+# - Initialised the keyring (see pacman-init, but roughly `pacman-key --init && pacman-key --populate archlinuxarm`)
+# - Fetched latest package database and upgraded (`pacman -Syu`)
+# - Installed python (`pacman -S python`)
+
 ansible-playbook playbooks/pi-bootstrap.yaml
