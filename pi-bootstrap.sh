29e59739bbdbbb12a014b14afc1b25c930463d5e
Alex Williams <lexw@live.co.uk>
2021-06-22 22:37:33 +0100

    IPv6 was overrated anyway...

--- pi-bootstrap.sh
+++ pi-bootstrap.sh
@@ -4,6 +4,8 @@
 # Pre-tasks:
 # - Installed base OS onto card
 # - Have SSH'd to it at least once (sshpass doesn't support missing hostkeys)
+
+# and then if auto-bootstrapping doesn't work...
 # - Initialised the keyring (see pacman-init, but roughly `pacman-key --init && pacman-key --populate archlinuxarm`)
 # - Fetched latest package database and upgraded (`pacman -Syu`)
 # - Installed python (`pacman -S python`)
