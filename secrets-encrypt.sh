8d380b92a3fdf1a61f4cf992a724e5997d404c65
Alex Williams <lexw@live.co.uk>
2025-01-31 16:57:35 +0000

    Add alerting

--- secrets-encrypt.sh
+++ secrets-encrypt.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
-tar cJf secrets.tar.xz playbooks/files/monitoring/config/homepower/credentials.yaml
+tar cJf secrets.tar.xz \
+    playbooks/files/monitoring/config/homepower/credentials.yaml \
+    playbooks/files/monitoring/config/alertmanager/bot_token_file.txt
 gpg --yes --symmetric -a -o secrets.tar.xz.asc secrets.tar.xz
 rm -f secrets.tar.xz
