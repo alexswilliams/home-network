4165870822309727109b7002a8eccd7c659ddad9
Alex Williams <lexw@live.co.uk>
2025-06-20 21:53:03 +0100

    Add Trmnl Renderer

--- secrets-encrypt.sh
+++ secrets-encrypt.sh
@@ -4,6 +4,7 @@ set -ex
 
 tar cJf secrets.tar.xz \
     playbooks/files/monitoring/config/homepower/credentials.yaml \
-    playbooks/files/monitoring/config/alertmanager/bot_token_file.txt
+    playbooks/files/monitoring/config/alertmanager/bot_token_file.txt \
+    playbooks/files/monitoring/config/prom-trmnl-renderer/credentials
 gpg --yes --symmetric -a -o secrets.tar.xz.asc secrets.tar.xz
 rm -f secrets.tar.xz
